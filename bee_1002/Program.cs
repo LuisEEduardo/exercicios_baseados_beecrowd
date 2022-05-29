85e8ce76f3c02b3eac414643cf62b1a63d3ff19b
LuisEEduardo <luiseduardolemos282@gmail.com>
2022-05-29 19:28:35 -0300

    Finalizando exercício 1002

---
+++ bee_1002/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace bee_1002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.Write("Insira o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine());
+            var area = CalculoDeAreaCirculo(raio);
+            Console.WriteLine(string.Format("Valor da área => {0:F2}", area));
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+        public static double CalculoDeAreaCirculo(double raio)
+        {
+            double pi = 3.14159;
+            return pi * Math.Pow(raio, 2);
+        }
+
+    }
+}
